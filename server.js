919de9ae9fcd8bc12cd3dae4e4becf675cc3df19
kingathurofficial <mufarohwenhira@gmail.com>
2026-04-25 16:36:59 +0200

    Add Express server for Render deployment

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Middleware
+app.use(express.static(path.join(__dirname, '.')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
